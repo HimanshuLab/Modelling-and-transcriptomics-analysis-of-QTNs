7239e1ba7a09890388cc931f8201030569a9f000
Srijith Sasikumar <86455247+bt19d403@users.noreply.github.com>
2024-10-08 21:35:49 +0530

    Update CreateSNPSpecificModel.m

--- CreateSNPSpecificModel.m
+++ CreateSNPSpecificModel.m
@@ -23,8 +23,6 @@ expData.genes = table2cell(geneExpData(:,1));
 context = geneExpData.Properties.VariableNames;
 context(:,1) = [];
 expData.context = context;
-contexts = {'C1','C2','C3','C4','C5','C6','C7','C8','C9',...
-        'C10','C11','C12','C13','C14','C15','C16'};
 % for LocalGini Thresholding
 if strcmp(threshold, 'LocalGini')
     
